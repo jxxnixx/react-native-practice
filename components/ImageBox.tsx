95e2660f96a364872123eedc956b6e03cad976c2
jxxnixx <dkepffk10@gmail.com>
2025-01-24 17:28:15 +0900

    feat: ImageBackground Practice

--- components/ImageBox.tsx
+++ components/ImageBox.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { View, StyleSheet, ScrollView, Platform, Image } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  Platform,
+  ImageBackground,
+  Text,
+} from 'react-native';
 
 const squareImages = {
   1: require('@/assets/images/square/square1.webp'),
@@ -16,10 +23,16 @@ const rectangleImages = {
   6: require('@/assets/images/rectangle/rectangle6.webp'),
 } as const;
 
-const SquareBox = ({ num }: { num: 1 | 2 | 3 }) => {
+const SquareBox = ({ num, text }: { num: 1 | 2 | 3; text?: string }) => {
   return (
     <View style={styles.imagebox1}>
-      <Image source={squareImages[num]} style={styles.image} />
+      <ImageBackground
+        source={squareImages[num]}
+        style={styles.image}
+        imageStyle={styles.imageStyle}
+      >
+        <Text style={styles.boxText}>{text}</Text>
+      </ImageBackground>
     </View>
   );
 };
@@ -27,14 +40,22 @@ const SquareBox = ({ num }: { num: 1 | 2 | 3 }) => {
 const RectangleBox = ({
   ratio,
   num,
+  text,
 }: {
   ratio: 2 | 3;
   num: 1 | 2 | 3 | 4 | 5 | 6;
+  text?: string;
 }) => {
   const imageBoxStyles = ratio === 2 ? styles.imagebox2 : styles.imagebox3;
   return (
     <View style={imageBoxStyles}>
-      <Image source={rectangleImages[num]} style={styles.image} />
+      <ImageBackground
+        source={rectangleImages[num]}
+        style={styles.image}
+        imageStyle={styles.imageStyle}
+      >
+        <Text style={styles.boxText}>{text}</Text>
+      </ImageBackground>
     </View>
   );
 };
@@ -44,39 +65,46 @@ const ImageBox = () => {
     <ScrollView
       style={styles.scrollView}
       contentContainerStyle={styles.imageboxContainer}
-      showsVerticalScrollIndicator
+      showsVerticalScrollIndicator={true}
     >
+      {/* 첫 번째 Row */}
       <View style={styles.rowContainer}>
-        <SquareBox num={1} />
-        <SquareBox num={2} />
-        <SquareBox num={3} />
-        <SquareBox num={1} />
-        <SquareBox num={2} />
-        <SquareBox num={3} />
-        <SquareBox num={1} />
-        <SquareBox num={2} />
-        <SquareBox num={3} />
-        <SquareBox num={1} />
-        <SquareBox num={2} />
-        <SquareBox num={3} />
+        <SquareBox num={1} text="F1 S1" />
+        <SquareBox num={2} text="F1 S2" />
+        <SquareBox num={3} text="F1 S3" />
+        <SquareBox num={1} text="F1 S1" />
+        <SquareBox num={2} text="F1 S2" />
+        <SquareBox num={3} text="F1 S3" />
+        <SquareBox num={1} text="F1 S1" />
+        <SquareBox num={2} text="F1 S2" />
+        <SquareBox num={3} text="F1 S3" />
+        <SquareBox num={1} text="F1 S1" />
+        <SquareBox num={2} text="F1 S2" />
+        <SquareBox num={3} text="F1 S3" />
       </View>
+
+      {/* 두 번째 Row */}
       <View style={styles.rowContainer}>
-        <SquareBox num={3} />
-        <RectangleBox ratio={2} num={1} />
-        <SquareBox num={2} />
-        <RectangleBox ratio={2} num={2} />
+        <SquareBox num={3} text="F1 S3" />
+        <RectangleBox ratio={2} num={1} text="F2 R1" />
+        <SquareBox num={2} text="F1 S2" />
+        <RectangleBox ratio={2} num={2} text="F2 R2" />
       </View>
+
+      {/* 세 번째 Row */}
       <View style={styles.rowContainer}>
-        <RectangleBox ratio={2} num={2} />
-        <SquareBox num={2} />
-        <RectangleBox ratio={3} num={5} />
-        <RectangleBox ratio={2} num={3} />
+        <RectangleBox ratio={2} num={2} text="F2 R2" />
+        <SquareBox num={2} text="F1 S2" />
+        <RectangleBox ratio={3} num={5} text="F3 R5" />
+        <RectangleBox ratio={2} num={3} text="F2 R3" />
       </View>
+
+      {/* 네 번째 Row */}
       <View style={styles.rowContainer}>
-        <SquareBox num={2} />
-        <SquareBox num={3} />
-        <SquareBox num={2} />
-        <RectangleBox ratio={3} num={5} />
+        <SquareBox num={2} text="F1 S2" />
+        <SquareBox num={3} text="F1 S3" />
+        <SquareBox num={2} text="F1 S2" />
+        <RectangleBox ratio={3} num={5} text="F3 R5" />
       </View>
     </ScrollView>
   );
@@ -88,17 +116,14 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   imageboxContainer: {
-    flex: 1,
-    height: 800,
+    flexGrow: 1,
+    width: '100%',
     padding: 10,
-    // backgroundColor: 'white',
     borderRadius: 10,
     borderWidth: 1,
     borderColor: 'lightgray',
   },
   rowContainer: {
-    flex: 1,
-    position: 'relative',
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingVertical: 5,
@@ -106,26 +131,29 @@ const styles = StyleSheet.create({
   },
   imagebox1: {
     flex: 1,
-    backgroundColor: 'skyblue',
-    borderRadius: 10,
-    marginInline: 5,
+    marginHorizontal: 5, // 요소 간의 간격 조절
+  },
+  imagebox2: {
+    flex: 2,
+    marginHorizontal: 5,
+  },
+  imagebox3: {
+    flex: 3,
+    marginHorizontal: 5,
   },
   image: {
     width: '100%',
     height: '100%',
+
+    padding: 8,
+  },
+  imageStyle: {
     borderRadius: 10,
   },
-  imagebox2: {
-    flex: 2,
-    backgroundColor: 'steelblue',
-    borderRadius: 10,
-    marginInline: 5,
-  },
-  imagebox3: {
-    flex: 3,
-    backgroundColor: 'navy',
-    borderRadius: 10,
-    marginInline: 5,
+  boxText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
