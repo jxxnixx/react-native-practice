5d8ba7b2d17f221fbaea61b3b8c70f761f51ee3b
jxxnixx <dkepffk10@gmail.com>
2025-01-24 17:34:10 +0900

    feat: Image + Svg + Header Practice

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,49 @@
 import { Stack } from 'expo-router';
+import { Image, StyleSheet, View, Text } from 'react-native';
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerTitle: 'Cat Growing Game' }}>
-      <Stack.Screen name="Cat" options={{ headerShown: true }} />
+    <Stack
+      screenOptions={{
+        headerLeft: () => (
+          <Image
+            source={require('@/assets/images/icons/star.svg')}
+            style={[styles.svgIcon, { marginInline: 6 }]}
+          />
+        ),
+        headerTitle: () => (
+          <View style={styles.titleWrapper}>
+            <Text style={styles.title}>Space</Text>
+            <Image
+              source={require('@/assets/images/icons/moon.svg')}
+              style={[styles.svgIcon, { marginInline: 6 }]}
+            />
+          </View>
+        ),
+        headerRight: () => (
+          <Image
+            source={require('@/assets/images/icons/planet.svg')}
+            style={styles.svgIcon}
+          />
+        ),
+      }}
+    >
+      {/* <Stack.Screen name="Cat" options={{ headerShown: true }} /> */}
     </Stack>
   );
 }
+
+const styles = StyleSheet.create({
+  titleWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  svgIcon: {
+    width: 24,
+    height: 24,
+  },
+});
